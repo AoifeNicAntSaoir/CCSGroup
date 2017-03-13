569ecdce0ddff36e55c9f5ebe6a1583bc8941d18
shnclifford <patrickshaneclifford@gmail.com>
2017-03-13 12:13:05 +0000

    now somewhat functional

--- firstAttempt.py
+++ firstAttempt.py
@@ -1,8 +1,9 @@
-from pybrain.datasets.supervised import SupervisedDataSet
-from pybrain.tools.shortcuts import buildNetwork
+from pybrain.datasets import ClassificationDataSet
+from pybrain.utilities import percentError
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.structure import FeedForwardNetwork, LinearLayer, SigmoidLayer, FullConnection
 from sklearn.datasets import load_digits
+import matplotlib.pyplot as plt
 import cv2
 
 
@@ -32,9 +33,9 @@ t = loadImage('shanesC.png')
 net = FeedForwardNetwork()
 
 #create layers for FFN
-inLayer = LinearLayer(len(t))
+inLayer = LinearLayer(len(t)) #sets up the number of nodes based on 'length' of the loaded image
 hiddenLayer = SigmoidLayer(len(t))
-outLayer = LinearLayer(1)
+outLayer = LinearLayer(10)#you need ten outputs - one for each digit(0,1,2,3 etc)
 
 # add layers to FFN
 net.addInputModule(inLayer)
@@ -53,16 +54,38 @@ net.sortModules()
 print net
 
 
-
-
-# daSet = SupervisedDataSet(len(t),1)
-
+digits = load_digits()
+X,y = digits.data, digits.target
 
 
 
-# for inpt, target in ds
-# print inpy, target
+print (X.shape)
 
-# trainer = BackpropTrainer(net,daSet)
 
-# trainer.train()
+plt.gray()
+plt.matshow(digits.images[2])
+plt.show()
+
+
+
+daSet = ClassificationDataSet(len(t),1)
+for k in xrange(len(X)):
+    daSet.addSample(X.ravel()[k],y.ravel()[k])
+
+
+testData, trainData = daSet.splitWithProportion(0.25)
+
+
+trainData._convertToOneOfMany( )
+testData._convertToOneOfMany( )
+#for inpt, target in daSet:
+   # print inpt, target
+
+trainer = BackpropTrainer(net, dataset=trainData, momentum=0.1, learningrate=0.01, verbose=True)
+
+trainer.trainEpochs(50)
+print 'Percent Error on Test dataset: ' , percentError( trainer.testOnClassData (
+           dataset=testData )
+           , testData['class'] )
+
+trainer.train()
