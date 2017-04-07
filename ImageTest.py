6dd19a19a3983e434abf344b8f51aa39a3dfaf5d
shnclifford <patrickshaneclifford@gmail.com>
2017-04-07 08:32:12 +0100

    updated files

--- ImageTest.py
+++ ImageTest.py
@@ -6,26 +6,24 @@ from scipy import io
 # load data
 data = io.loadmat('data_mnist.mat')  # load the data
 
-X = data['X']
-Y = data['y']  # split to x and y
+X = data['X']  # store the parts of the data labeled X so that we can get its size and shape
 
 c = random.randint(0, X.shape[0])  # get random index
 
-c2 = X[c, :] #  get the data stored at that index
+c2 = X[c, :]  # get the data stored at that index
 
-
-#  show the in a graph
+#  show the digit in a graph
 m, n = shape(X)
 image = array(X[c, 0:n])
 plt.imshow((image.reshape(20, 20)).T, cmap='Greys')
 plt.show()
 
 # read the saved network from the file
-net = NetworkReader.readFrom('test_temp.xml')
+net = NetworkReader.readFrom('good_net.xml')
 
 # pass the test image through the neural net
 prediction = net.activate(c2)
 # get the value with the highest probability
 p = argmax(prediction, axis=0)
 print(prediction)
-print("predicted output is \t" + str(p))
+print("The digit should be : \t" + str(p))
--- neural_network_digit_recognition_group_proj.py
+++ neural_network_digit_recognition_group_proj.py
@@ -1,7 +1,8 @@
 from random import randint, shuffle
 import os
 from numpy import *
-from pybrain.datasets import ClassificationDataSet
+import numpy as np
+from pybrain.datasets import SupervisedDataSet
 from pybrain.structure import SigmoidLayer, SoftmaxLayer
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.tools.shortcuts import buildNetwork
@@ -10,26 +11,26 @@ from pybrain.tools.xml import NetworkWriter
 from pybrain.utilities import percentError
 from scipy import io
 import matplotlib.pyplot as plt
-import matplotlib.pyplot as pl2
+import matplotlib.pyplot as plt2
+
 
 def plotData(image):
-    # plots the input data '''
-    # the image matrix will have to be transposed to be viewed correcty
-    # cmap shows the color map
-    plt.imshow(image.T, cmap='Greys')
-    plt.show()
+	# plots the input data '''
+	# the image matrix will have to be transposed to be viewed correctly
+	# cmap shows the color map
+	plt.imshow(image.T, cmap='Greys')
+	plt.show()
 
 
-
-def convert_to_one_of_many(Y):
+def convert_to_one_of_many(y):
 	# converts Y to one of many types
 	# or one output per label
-	rows, cols = Y.shape
-	classes = unique(Y).size  # should get 10 classes
+	rows, cols = y.shape
+	classes = unique(y).size  # should get 10 classes
 	new_y = zeros((rows, classes))
 
 	for i in range(0, rows):
-		new_y[i, Y[i]] = 1
+		new_y[i, y[i]] = 1
 
 	return new_y
 
@@ -38,20 +39,22 @@ def convert_to_one_of_many(Y):
 digits = io.loadmat('data_mnist.mat')
 
 
-
 # making X and Y numpy arrays
 X = digits['X']
 Y = digits['y']
 
+
 Y[Y == 10] = 0  # 0 has the 10th position, this line gives it the 0th position
+
 
 num_of_labels = unique(Y).size  # gets your 10 labels/outputs
 
 # build the dataset
 
 num_of_examples, size_of_example = X.shape
+
 # convert the test data to one of many (10)
-Y = convert_to_one_of_many(Y)
+Y = convert_to_one_of_many(Y) # splits the data into 10 outputs
 
 # separating training and test data sets
 
@@ -63,13 +66,13 @@ Y = X1[:, size_of_example: X1.shape[1]]
 
 # add the contents of digits to a dataset
 
-train_data = ClassificationDataSet(size_of_example, num_of_labels)
-test_data = ClassificationDataSet(size_of_example, num_of_labels)
+train_data = SupervisedDataSet(size_of_example, num_of_labels)
+test_data = SupervisedDataSet(size_of_example, num_of_labels)
 
 data_split = int(num_of_examples * 0.9)
 
 for i in range(0, data_split):
-	train_data.addSample(X[i, :], Y[i, :])
+	train_data.addSample(X[i, :], Y[i, :]) #add .9 of the data to the ds
 
 # setting the field names
 train_data.setField('input', X[0:data_split, :])
@@ -81,11 +84,12 @@ for i in range(data_split, num_of_examples):
 test_data.setField('input', X[data_split:num_of_examples, :])
 test_data.setField('target', Y[data_split:num_of_examples, :])
 
-if os.path.isfile('dig.xml'):
-	net = NetworkReader.readFrom('dig.xml')
+#if os.path.isfile('extra_layers.xml'):
+	#net = NetworkReader.readFrom('extra_layers.xml')
 
-else:
-	net = buildNetwork(size_of_example, 185, num_of_labels, hiddenclass=SigmoidLayer,
+#else:
+net = buildNetwork(size_of_example, int(size_of_example), int(size_of_example) / 5, num_of_labels,
+					   hiddenclass=SigmoidLayer,
 					   outclass=SoftmaxLayer)
 
 net.sortModules()
@@ -96,12 +100,12 @@ test_input = X[test_index]
 real_train = train_data['target'].argmax(axis=1)
 real_test = test_data['target'].argmax(axis=1)
 
-EPOCHS = 20
+EPOCHS = 3
 
-trainer = BackpropTrainer(net, dataset=train_data, momentum=0.3,learningrate=0.01, verbose=False)
+trainer = BackpropTrainer(net, dataset=train_data, momentum=0.41, learningrate=0.03, verbose=False)
 
 trainResultArr = []
-epochs =  []
+epochs = []
 testResultArr = []
 
 for i in range(EPOCHS):
@@ -109,7 +113,7 @@ for i in range(EPOCHS):
 	trainer.trainEpochs(1)
 
 	outputTrain = net.activateOnDataset(train_data)
-	outputTrain = outputTrain.argmax(axis=1)
+	outputTrain = outputTrain.argmax(axis=1) # takes the output with the largest prob
 	trainResult = percentError(outputTrain, real_train)
 
 	outputTest = net.activateOnDataset(test_data)
@@ -119,24 +123,26 @@ for i in range(EPOCHS):
 	finalTrainResult = 100 - trainResult
 	finalTestResult = 100 - testResult
 
-	print "Epoch: " + str(i + 1) + "\tTraining set accuracy: " +  str(finalTrainResult) + "\tTest set accuracy: " + str(finalTestResult)
-	#getStatistics(	)
+	print "Epoch: " + str(i + 1) + "\tTraining set accuracy: " + str(finalTrainResult) + "\tTest set accuracy: " + str(
+		finalTestResult)
+	# getStatistics(	)
 
-	trainResultArr.append((finalTestResult))
-	testResultArr.append((finalTrainResult))
-	epochs.append((i))
+	trainResultArr.append(finalTestResult)
+	testResultArr.append(finalTrainResult)
+	epochs.append(i)
 
 prediction = net.activate(test_input)
+
 # returns the index of the highest value down the columns
 p = argmax(prediction, axis=0)
 
-NetworkWriter.writeToFile(net, 'dig.xml')
+NetworkWriter.writeToFile(net, 'extra_layers.xml')
 
-plt.plot(epochs,trainResultArr)
-plt.plot(epochs,testResultArr)
+plt.plot(epochs, trainResultArr)
+plt.plot(epochs, testResultArr)
 plt.title('Training Result (Orange) vs Test Result of ANN (Blue)')
 plt.xlabel('Epochs')
 plt.ylabel('Accuracy %')
-plt.legend(['Training Result', 'Test Result'])
 
 plt.show()
+
